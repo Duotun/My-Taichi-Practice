88df20e5d9052096710e4c7b2abfde29e4253b7e
Duotun <yonngwang@outlook.com>
2023-02-08 14:37:34 +0800

    Learn Dynamic SNode Examples

--- Misc/DynamicSNode_MSquare.py
+++ Misc/DynamicSNode_MSquare.py
@@ -1,5 +1,9 @@
 #only support cpu and cuda backend for now
 import taichi as ti 
+import taichi.math as tm 
+import numpy as np
+import time  # for random generators
+
 
 ti.init(arch=ti.cuda);
 
@@ -24,14 +28,41 @@ def clear_data():
 
 
 #Marching Squares
+#define local space coordinate (from interpolation)
+#add grid positoin -> global space coordinate
+#utilize Dynamic Node to form a complete update each frame in GPU of parallel ways
+
 #screen rendering init
 Width, Height = 800, 600;
 resolution = (Width, Height);
 grid_size = 8;
+level = 0.15 # draw contour of isofunc = level
 iTime = ti.field(float, shape=());
+pixels = ti.Vector.field(3, float, shape = resolution);
 
-#create edge tables for 16 cases
+#create edge tables for 18 cases  (2 cases are combined to solve ambiguity)
+#becareful with the ambiguity (saddle point)
+edge_table_np = np.array([   #follow the encode sequence (0 -15)
+    [[-1, -1], [-1, -1]],
+    [[3, 0], [-1, -1]],
+    [[0, 1],[-1, -1]],
+    [[1, 3], [-1, -1]],
+    [[1, 2], [-1, -1]],
+    [[0, 1], [2, 3]],
+    [[0, 2], [-1, -1]],
+    [[2, 3], [-1, -1]],
+    [[2,3],[-1,-1]],
+    [[0, 2], [-1, -1]],
+    [[0, 3],[1,2]],
+    [[1, 2],[-1, -1]],
+    [[1, 3],[-1, -1]],
+    [[0, 1], [-1, -1]],
+    [[3, 0], [-1, -1]],  #14
+    [[-1, -1],[-1,-1]]   #15
+    ], dtype = np.int32);
+
 edge_table = ti.Matrix.field(2, 2, int, 16);
+edge_table.from_numpy(edge_table_np);
 
 vec2 = ti.math.vec2;
 Edge = ti.types.struct(p0=vec2, p1=vec2);   #declare struct type
@@ -41,8 +72,151 @@ D_Node.place(edges);
 
 #assume unit length - 1, and square's range from [-10, 10]
 
+@ti.func
+def hash22(p):
+    n = tm.sin(tm.dot(p, tm.vec2(41, 289)))
+    p = tm.fract(tm.vec2(262144, 32768) * n)
+    return tm.sin(p * 6.28 + iTime[None])   # introduce time variable for random generator
 
+
+@ti.func
+def noise(p):
+    ip = tm.floor(p)
+    p -= ip
+    v = tm.vec4(tm.dot(hash22(ip), p),
+                tm.dot(hash22(ip + tm.vec2(1, 0)), p - tm.vec2(1, 0)),
+                tm.dot(hash22(ip + tm.vec2(0, 1)), p - tm.vec2(0, 1)),
+                tm.dot(hash22(ip + tm.vec2(1, 1)), p - tm.vec2(1, 1)))
+    p = p * p * p * (p * (p * 6 - 15) + 10)
+    return tm.mix(tm.mix(v.x, v.y, p.x), tm.mix(v.z, v.w, p.x), p.y)    #mix ->linear interpolation
+
+@ti.func
+def isofunc(p):
+    return noise(p/4 +1);
+
+
+#get the vertex position of the unit grid [[0, 1], [2, 3]] counter-clock 2D grid
+#values refer to the isovalue on the grid, isovalue is the level (contour)
+@ti.func
+def get_vertex(vertex_id, values, isovalue):   #isovalu for the interpolatoin
+    v = tm.vec2(0);
+    square = [tm.vec2(0), tm.vec2(0, 1), tm.vec2(1, 1), tm.vec2(1, 0)];
+    if vertex_id == 0:
+        v = interp(square[0], square[1],values.x, values.y, isovalue);
+    elif vertex_id == 1:
+        v = interp(square[1], square[2],values.y, values.z, isovalue);
+    elif vertex_id == 2:
+        v = interp(square[2], square[3],values.z, values.w, isovalue);
+    elif vertex_id == 3:
+        v = interp(square[3], square[0],values.w, values.x, isovalue);
+    return v;
+    
+#used for define the position of iso lines
+@ti.func
+def interp(p1, p2, v1, v2, interp_val):
+    return tm.mix(p1, p2, (interp_val - v1)/(v2-v1));
+
+
+#update marching_square with dynamic nodes each frame
+#and in parallel way
+@ti.kernel
+def marching_square():
+    edges.deactivate();    #clear the dynamic nodes
+    x_range = tm.ceil(grid_size * Width/Height, int);
+    y_range = grid_size;
+    
+    for i, j in ti.ndrange((-x_range, x_range),(-y_range, y_range)):
+        t_id = 0;   # for accessing edge table
+        grid_values = tm.vec4(isofunc(tm.vec2(i, j)), isofunc(tm.vec2(i, j+1)),
+                              isofunc(tm.vec2(i+1, j+1)), isofunc(tm.vec2(i+1, j)));   #for coordinates
+        # form the bit-mapping
+        if grid_values.x > level:
+            t_id |=1;
+        if grid_values.y > level:
+            t_id |=2;
+        if grid_values.z > level:
+            t_id |=4;
+        if grid_values.w > level:
+            t_id |=8;
+        
+        #Fix the ambiguity for case 5 and 10
+        if t_id == 5 or t_id ==10:
+            center_val = isofunc(tm.vec2(i+0.5, j+0.5));
+            if center_val > level:
+                t_id = 15 - t_id;
+        
+        #check whether to expand the edges
+        for k in ti.static(range(2)):
+            if edge_table[t_id][k, 0] != -1:
+                ind_1 = edge_table[t_id][k, 0];
+                ind_2 = edge_table[t_id][k, 1];
+                p0 =  tm.vec2(i, j) + get_vertex(ind_1, grid_values, level);
+                p1 =  tm.vec2(i, j) + get_vertex(ind_2, grid_values, level);
+                edges.append(Edge(p0, p1));
+        
+#dseg -> calculate the vertical distance to the edge
+@ti.func
+def dseg(p, a, b):
+    p -=a;
+    b -=a;
+    h = tm.clamp(tm.dot(p, b)/tm.dot(b, b), 0.0, 1.0);
+    return tm.length(p - h*b);
+
+#draw grid, edges, and iso areas
+#follow the grid position to define the final color
+#map the marching square to the renderer pages
+@ti.kernel
+def render():
+    for i, j in pixels:
+        p = (2 * tm.vec2(i, j) - tm.vec2(resolution))/Height;  #[-W/H, W/H] X [-1, 1]
+        p *= grid_size
+        #p.y +=1.2;
+        # Background Color
+        col = tm.vec3(0.3, 0.6, 0.8);  
+        if isofunc(p) > level:    #iso - range, no countour lines interpolated
+            col = tm.vec3(1, 0.8, 0.3)
+            
+        # Draw background grid (black lines), manhattan distance to the grid
+        q = abs(tm.fract(p) - 0.5);
+        dd = 0.5 - q;     
+        dgrid = ti.min(dd.x, dd.y);
+        col = tm.mix(col, tm.vec3(0), 1-tm.smoothstep(0,0.04, dgrid-0.02))  
+        
+        # Draw edges
+        dedge = 1e5;
+        dv = 1e5;
+        
+        for k in range(edges.length()):
+            s, e = edges[k].p0, edges[k].p1;
+            dedge = ti.min(dedge, dseg(p, s, e));  #close to the edges
+            dv = ti.min(dv, tm.length(p-s), tm.length(p-e));
+        
+        col = tm.mix(col, tm.vec3(1, 0, 0),
+                     1-tm.smoothstep(0, 0.05, dedge-0.02));
+        
+        # Draw small circles at the vertices
+        # With boundary ways
+        col = tm.mix(col, tm.vec3(0.), 1-tm.smoothstep(0, 0.05, dv - 0.1));
+        col = tm.mix(col, tm.vec3(1.), 1-tm.smoothstep(0, 0.05, dv - 0.08));
+         
+        pixels[i, j] = tm.sqrt(tm.clamp(col, 0,1))   #gamma-correction
+
+# For screen show
+t0 = time.perf_counter();
+gui = ti.GUI('Marching Square (2D)', res = resolution, fast_gui=True);    
+
+def screen_show():
+    while gui.running and not gui.get_event(gui.ESCAPE):
+        iTime[None] = time.perf_counter() - t0;
+        marching_square();
+        render()
+        gui.set_image(pixels);
+        gui.show();
 
 if __name__ == "__main__":
-    add_data();
-    clear_data();
+    #basic test
+    #add_data();
+    #clear_data();
+    
+    #marching square
+    screen_show();
--- Misc/DynamicSNode_OIT.py
+++ Misc/DynamicSNode_OIT.py
@@ -1,1 +1,169 @@
 #only support cpu and cuda backend for now
+#OIT - Sequence-unrelated transparency rendering
+import taichi as ti
+import taichi.math as tm 
+
+ti.init(arch=ti.cuda)
+
+#Screen Show Init
+res = (1000, 1000);
+color_buffer = ti.Vector.field(3, dtype=ti.f32, shape=res);
+
+eps = 1e-4;
+inf = 1e10;
+fov = 0.5;
+aspect_ratio = res[0] / res[1];
+
+#Scene Data
+alpha_min = 0.2;
+alpha_width = 0.3;
+cam_pos = tm.vec3(0., 0., 5.);
+background_color = tm.vec4(0.2, 0.2, 0.2, 1.);
+
+Line = ti.types.struct(pos=tm.vec3, dir=tm.vec3);
+Sphere = ti.types.struct(center=tm.vec3, radius=float, color=tm.vec4);
+spheres = Sphere.field();   # spheres as 3d object to construct the scene
+ti.root.dynamic(ti.i, 1024, chunk_size=64).place(spheres);
+
+ColorWithDepth = ti.types.struct(color=tm.vec4, depth=float);
+colors_in_pixel = ColorWithDepth.field();
+ti.root.dense(ti.ij, res).dynamic(ti.k, 2048, chunk_size=64).place(colors_in_pixel);
+
+
+#rendering utilize one-pass gooch NPR(no edge stuff)
+
+@ti.func
+def gooch_lighting(normal: ti.template()):
+    light = tm.normalize(tm.vec3(-1., 2., 1.));
+    warmth = normal*light*0.5 + 0.5;
+    return tm.mix(tm.vec3(0., 0.25, 0.75), tm.vec3(1., 1., 1.), warmth);
+
+#assign color to objects (spheres this case)
+@ti.func
+def shading(color, normal: ti.template()):
+    inColor = color.rgb * gooch_lighting(normal);
+    alpha = tm.clamp(alpha_min + color.a * alpha_width, 0., 1.);
+    return tm.vec4(inColor, alpha);
+
+
+#assume rays from the image coordinates (camera_pos, considered as well)
+#assume image is one-unit in the front of camera
+@ti.func  
+def gen_cam_ray(u, v):
+    #y-[-1, 1], x - [-aspect, aspect], z - related to the fov / focal_length
+    ray_dir = tm.vec3((2 * (u+0.5)/res[0]-1)* aspect_ratio, 2 * (v+0.5)/res[1]-1., -1.0/fov);
+    ray_dir = tm.normalize(ray_dir);
+    return Line(pos=cam_pos, dir=ray_dir);
+
+#intersection process
+#line-sphere intersections
+@ti.func 
+def intersect_sphere(line: Line, sphere: ti.template()):
+    colorA = tm.vec4(0.);
+    colorB = tm.vec4(0.);
+    dist_1 = inf;
+    dist_2 = inf;
+    dl = sphere.center - line.pos;
+    dl_dis = dl.dot(dl);
+    per_pendicular = dl.dot(line.dir);  #small means per_pendicular
+    r_dis = sphere.radius * sphere.radius
+    out_of_sphere = (dl_dis > r_dis);
+    is_intersection = True;   #default is true
+
+    #check perpendicular case / or reversed directions
+    if -eps < dl_dis -r_dis <eps:   #if very close
+        if -eps < per_pendicular < eps:
+            is_intersection = False;
+        out_of_sphere = (per_pendicular < 0.);   #reversed dir or not
+    
+    if per_pendicular < 0. and out_of_sphere:
+        is_intersection = False 
+    
+    #check intersection case (two-potential points)
+    if is_intersection == True:
+        in_radius =  -dl_dis +r_dis + per_pendicular * per_pendicular;
+        if in_radius >=0.0:
+            in_radius = tm.sqrt(in_radius);
+            t1 = per_pendicular -  in_radius;
+            if t1>0:
+                hit_pos1 = line.pos + line.dir *t1;
+                dist_1 = t1;
+                normal1 = tm.normalize(hit_pos1 - sphere.center);
+                colorA = shading(sphere.color, normal1);
+            t2 = per_pendicular + in_radius;
+            if t2>0:
+                hit_pos2 = line.pos + line.dir *t2;
+                dist_2 = t2;
+                normal2 = tm.normalize(hit_pos2 - sphere.center);
+                colorB = shading(sphere.color, normal2);
+    return ColorWithDepth(color=colorA, depth = dist_1), ColorWithDepth(color=colorB, depth=dist_2);
+
+#calculate interseciton each pixel, expand the colors_in_pixel
+@ti.func
+def get_intersection_pixel(u,v):
+    line = gen_cam_ray(u, v);
+    colors_in_pixel[u, v].deactivate();
+    for i in range(spheres.length()):
+        hit1, hit2 = intersect_sphere(line, spheres[i]);
+        if hit1.depth < inf:
+            colors_in_pixel[u,v].append(hit1);
+        if hit2.depth < inf:
+            colors_in_pixel[u,v].append(hit2);
+
+#sort the dynamic nodes in each pixel case (ascending order)
+@ti.func 
+def bubble_sort(u,v):
+    plen = colors_in_pixel[u, v].length();              
+    for i in range(plen-1):
+        for j in range(plen-i-1):    # pop the largets to the last
+            if colors_in_pixel[u,v,j].depth > colors_in_pixel[u, v, j+1].depth:
+                tmp = colors_in_pixel[u,v,j].depth;
+                colors_in_pixel[u,v,j].depth = colors_in_pixel[u,v,j+1].depth;
+                colors_in_pixel[u,v,j+1].depth = tmp;
+
+#blend color with depth sorted
+#weighted with color.a
+@ti.func
+def blend(color, base_color):
+    color.rgb += (1-color.a)*base_color.rgb * base_color.a 
+    color.a += (1-color.a) * base_color.a;
+    return color;
+
+#blend from the pure black and transparent (ascending with depth)
+@ti.func
+def get_color(u,v):
+     color = tm.vec4(0.0);
+     for i in range(colors_in_pixel[u,v].length()):
+         color = blend(color, colors_in_pixel[u, v, i].color);
+     color = blend(color, background_color);
+     gamma_corrected_color = tm.pow(color.rgb*color.a, 1./2.2);
+     #print(color.a)
+     color_buffer[u,v]= gamma_corrected_color;
+    
+
+#kernel stuff for parallel
+@ti.kernel
+def generate_spheres(n: ti.i32):
+    spheres.deactivate();
+    for i in range(n):
+        spheres.append(Sphere(center=tm.vec3(ti.random()*3 - 1.5, ti.random()*3 - 1.5, ti.random()*3 - 1.5), radius=ti.random()*0.2+0.1,color=tm.vec4(ti.random(), ti.random(), ti.random(), ti.random())));
+
+@ti.kernel
+def render():
+    for u,v in color_buffer:
+        get_intersection_pixel(u,v);
+        bubble_sort(u,v)
+        get_color(u,v)
+
+# simple gui to show the rendered image
+def screen_show():
+    gui = ti.GUI('OIT Rendering', res, fast_gui=True);
+    generate_spheres(256);   #prepare the sphere scene
+    render();
+    gui.set_image(color_buffer);
+    while gui.running:
+        gui.show();
+
+if __name__ == "__main__":
+    screen_show();
+
